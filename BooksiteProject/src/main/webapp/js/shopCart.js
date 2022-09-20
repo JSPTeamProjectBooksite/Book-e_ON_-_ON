8a0b4523c99b216bd1859c254993cea6693d6526
DKS2312 <dtg2312@gmail.com>
2022-09-20 18:26:27 +0900

    장바구니 페이지 기능 완료

--- BooksiteProject/src/main/webapp/js/shopCart.js
+++ BooksiteProject/src/main/webapp/js/shopCart.js
@@ -3,7 +3,6 @@ const checkBoxFalseImg = "source/ico/check_box_false.svg";
 
 $(function (){   
     // 장바구니에 있는 책을 누르면 버튼처럼 작동됨
-    // number input을 건드릴 땐 반응을 못하게 함
     $('.bookItem').mousedown( function(event){
         let targetType = $(event.target).attr('type');
         
@@ -22,9 +21,21 @@ $(function (){
     $('.bookItem').mouseup(function() { $(this).removeClass('bookItemClick') }); 
     $('.bookItem').mouseout(function() { $(this).removeClass('bookItemClick') });
 
-    $('.selectBookSaveBt').click(selectBookSaveBtnFun());
+    $('.selectBookSaveBtn').click(selectBookSaveBtnFun);
 });
 
 function selectBookSaveBtnFun(){
-    alert("하이");
+    alert("쿠키 설정");
+
+    setCookie("bookID", "99999999999999999999999999999999", 3);
+};
+
+
+
+// 쿠키 설정 setCookie('pop', 'event0405', 7); /* pop=event0405, 7일 뒤 만료됨 */
+function setCookie(name, value, exp) {
+    var date = new Date();
+    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
 }
+
