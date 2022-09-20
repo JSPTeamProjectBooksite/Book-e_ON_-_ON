1f60449f34636539676f10e81833d1b28d3eb530
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2022-09-20 15:50:16 +0900

    커미트

---
+++ BooksiteProject/src/main/java/org/iptime/booke/controller/ListPageController.java
@@ -0,0 +1,26 @@
+package org.iptime.booke.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListPageController
+ */
+@WebServlet("/list")
+public class ListPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/ListPage.jsp").forward(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
