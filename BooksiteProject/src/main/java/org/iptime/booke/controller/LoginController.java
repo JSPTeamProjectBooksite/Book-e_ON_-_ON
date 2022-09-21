08498cf1594f2244db3026de10f3c97a9774ebe1
jinyoungchoi1003 <samcokz@naver.com>
2022-09-21 11:44:18 +0900

    2st commit

--- BooksiteProject/src/main/java/org/iptime/booke/controller/LoginController.java
+++ BooksiteProject/src/main/java/org/iptime/booke/controller/LoginController.java
@@ -9,14 +9,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Login")
+@WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("./Login.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("./LoginPage.jsp");
 		dispatcher.forward(request, response);
 	}
 
