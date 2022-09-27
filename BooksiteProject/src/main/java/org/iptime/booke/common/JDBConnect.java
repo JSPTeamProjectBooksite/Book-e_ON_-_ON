58a49e49e7644a8aa71d2463270e0f0819367a2b
) <107034998+imyoungi@users.noreply.github.com>
2022-09-27 22:39:43 +0900

    Update JDBConnect.java

--- BooksiteProject/src/main/java/org/iptime/booke/common/JDBConnect.java
+++ BooksiteProject/src/main/java/org/iptime/booke/common/JDBConnect.java
@@ -17,41 +17,13 @@ public class JDBConnect {
 	public JDBConnect() {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
-
+			
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
 			String id = "c##green";
 			String pwd = "green1234";
 			con = DriverManager.getConnection(url, id, pwd);
 
 			System.out.println("DB 연결 성공(기본 생성자)");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public JDBConnect(String driver, String url, String id, String pwd) {
-		try {
-			Class.forName(driver);
-
-			con = DriverManager.getConnection(url, id, pwd);
-
-			System.out.println("DB 연결 성공(인수 생성자 1)");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public JDBConnect(ServletContext application) {
-		try {
-			String driver = application.getInitParameter("OracleDriver");
-			Class.forName(driver);
-
-			String url = application.getInitParameter("OracleURL");
-			String id = application.getInitParameter("OracleId");
-			String pwd = application.getInitParameter("OraclePwd");
-			con = DriverManager.getConnection(url, id, pwd);
-
-			System.out.println("DB 연결 성공(인수 생성자 2)");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
