9b3cdc7a179aa50ca9798ced3bec4a8d2757cf00
DKS2312 <dtg2312@gmail.com>
2022-10-05 21:55:22 +0900

    main 브랜치와 병합 준비

--- BooksiteProject/src/main/java/org/iptime/booke/dto/MemberDTO.java
+++ BooksiteProject/src/main/java/org/iptime/booke/dto/MemberDTO.java
@@ -1,54 +1,68 @@
 package org.iptime.booke.dto;
 
-import java.util.Date;
-
 public class MemberDTO {
-	private Long id;
 	private String name;
-	private Short genderId;
+	private String gender;
+	private String id;
 	private String password;
-	private String phoneNum;
+	private String phoneNumber;
 	private String address;
 	private String email;
-	private Date birth;
-	private Integer point;
-	private Short memberStateId;
-	private Date registerDate;
 	
-	
-	public MemberDTO() {}
+	public MemberDTO() {
+		
+	}
 	
 	//결제페이지
-	public MemberDTO(String name, Long id, String phoneNum, String address) {
+	
+	public MemberDTO(String name, String id, String phoneNumber, String address) {
 		this.name = name;
 		this.id = id;
-		this.phoneNum = phoneNum;
+		this.phoneNumber = phoneNumber;
 		this.address = address;
 		
 	}
 	
-	public MemberDTO(String name, Long id, String password, String phoneNum, String address) {
+	public MemberDTO(String name,String id, String password, String phoneNumber, String address) {
 		this.name = name;
 		this.id = id;
 		this.password = password;
-		this.phoneNum = phoneNum;
+		this.phoneNumber = phoneNumber;
 		this.address = address;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+//	public MemberDTO(String name, int gender, String id, String password, int phoneNumber, String address) {
+//		super();
+//		this.name = name;
+//		this.gender = gender;
+//		this.id = id;
+//		this.password = password;
+//		this.phoneNumber = phoneNumber;
+//		this.address = address;
+//	}
+	
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getPassword() {
@@ -59,12 +73,12 @@ public class MemberDTO {
 		this.password = password;
 	}
 
-	public String getPhoneNum() {
-		return phoneNum;
+	public String getPhoneNumber() {
+		return phoneNumber;
 	}
 
-	public void setPhoneNum(String phoneNum) {
-		this.phoneNum = phoneNum;
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
 	public String getAddress() {
@@ -75,6 +89,14 @@ public class MemberDTO {
 		this.address = address;
 	}
 
+//	public int getOrderQuantity() {
+//		return orderQuantity;
+//	}
+//
+//	public void setOrderQuantity(int orderQuantity) {
+//		this.orderQuantity = orderQuantity;
+//	}
+
 	public String getEmail() {
 		return email;
 	}
@@ -83,43 +105,6 @@ public class MemberDTO {
 		this.email = email;
 	}
 
-	public Date getBirth() {
-		return birth;
-	}
 
-	public void setBirth(Date birth) {
-		this.birth = birth;
-	}
-
-	public Integer getPoint() {
-		return point;
-	}
-
-	public void setPoint(Integer point) {
-		this.point = point;
-	}
-
-	public Short getMemberStateId() {
-		return memberStateId;
-	}
-
-	public void setMemberStateId(Short memberStateId) {
-		this.memberStateId = memberStateId;
-	}
-
-	public Date getRegisterDate() {
-		return registerDate;
-	}
-
-	public void setRegisterDate(Date registerDate) {
-		this.registerDate = registerDate;
-	}
-
-	public Short getGenderId() {
-		return genderId;
-	}
-
-	public void setGenderId(Short genderId) {
-		this.genderId = genderId;
-	}
+	
 }
