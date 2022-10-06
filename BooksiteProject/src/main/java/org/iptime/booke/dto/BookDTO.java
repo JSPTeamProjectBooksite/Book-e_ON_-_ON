55005f6932c9758f46927c115afdbcfc99807708
DKS2312 <dtg2312@gmail.com>
2022-10-06 12:56:31 +0900

    Book, Member DTO변경

--- BooksiteProject/src/main/java/org/iptime/booke/dto/BookDTO.java
+++ BooksiteProject/src/main/java/org/iptime/booke/dto/BookDTO.java
@@ -1,165 +1,127 @@
 package org.iptime.booke.dto;
 
+import java.util.Date;
+
 public class BookDTO {
-	Long bid;
-	String image;
-	String title;
-	String author;
-	String translator;
-	Long price;
-	Long deliveryFee;
-	String estimatedDeliveryDate;
-	Long totalPages;
-	Long weight;
-	Long isbn;
-	String bookCategoryId;
-	String introduce;
-	String introduceImage;
-	String publisher;
-	String publisherReview;
-	String contents;
-	Long visit;
-	String publicationDate;
-	String registDate;
-	String updateDate;
-	String ad;
-	
-	//선택수량
-	int select; //DB에 없으나, DTO의 인스턴스로 존재
+	private Long id;
+	private String coverImg;
+	private String title;
+	private Long authorId;
+	private String translator;
+	private Integer price;
+	private Integer deliveryFee;
+	private Integer estimatedDeliveryDate;
+	private Integer totalPages;
+	private Integer weight;
+	private Long isbn13;
+	private Long isbn10;
+	private Integer bookCategoryId;
+	private String introduce;
+	private String introduceImg;
+	private String publisher;
+	private String publisherReview;
+	private String contents;
+	private Integer visit;
+	private Integer quantity;
+	private String catchphrase;
+	private Date publicationDate;
+	private Date registDate;
+	private Date updateDate;
 	
 	/* dto 값 받을때 쓰는거 (복사해서 쓰세요.)
-	
-	BookDTO dto = new BookDTO();
-	
-	dto.setBid(rs.getLong(1));
-	dto.setImage(rs.getString(2));
-	dto.setTitle(rs.getString(3));
-	dto.setAuthor(rs.getString(4));
-	dto.setTranslator(rs.getString(5));
-	dto.setPrice(rs.getLong(6));
-	dto.setDeliveryFee(rs.getLong(7));
-	dto.setEstimatedDeliveryDate(rs.getString(8));
-	dto.setTotal_pages(rs.getLong(9));
-	dto.setWeight(rs.getLong(10));
-	dto.setIsbn(rs.getLong(11));
-	dto.setBookCategoryId(rs.getString(12));
-	dto.setIntroduce(rs.getString(13));
-	dto.setIntroduceImage(rs.getString(14));
-	dto.setPublisher(rs.getString(15));
-	dto.setPublisherReview(rs.getString(16));
-	dto.setContents(rs.getString(17));
-	dto.setVisit(rs.getLong(18));
-	dto.setPublicationDate(rs.getString(19));
-	dto.setRegistDate(rs.getString(20));
-	dto.setUpdateDate(rs.getString(21));
-	dto.setAd(rs.getString(22));
 	
 	return dto;
 	
 	*/
-	
-	public int getSelect() {
-		return select;
-	}
-
-	public void setSelect(int select) {
-		this.select = select;
-	}
-
-	// 장바구니 수량 22
-	Integer count;
 
 	public BookDTO() {}
 	
 	public void DTOPrintOut() {
-		System.out.println(bid);
-		System.out.println(image);
+		System.out.println(id);
+		System.out.println(coverImg);
 		System.out.println(title);
-		System.out.println(author);
+		System.out.println(authorId);
 		System.out.println(translator);
 		System.out.println(price);
 		System.out.println(deliveryFee);
-		System.out.println(estimatedDeliveryDate);
-		System.out.println(totalPages);
-		System.out.println(weight);
-		System.out.println(isbn);
+		System.out.println(isbn13);
+		System.out.println(isbn10);
 		System.out.println(bookCategoryId);
 		System.out.println(introduce);
-		System.out.println(introduceImage);
+		System.out.println(introduceImg);
 		System.out.println(publisher);
 		System.out.println(publisherReview);
 		System.out.println(contents);
 		System.out.println(visit);
+		System.out.println(quantity);
+		System.out.println(catchphrase);
 		System.out.println(publicationDate);
 		System.out.println(registDate);
 		System.out.println(updateDate);
-		System.out.println(ad);
 	}
 	
 	//결제페이지 사용
 	public void DTOPrintOut2() {
-		System.out.println(bid);
-		System.out.println(image);
+		System.out.println(id);
+		System.out.println(coverImg);
 		System.out.println(title);
 		System.out.println(price);
 		System.out.println(bookCategoryId);
-		System.out.println(select);
+		System.out.println(quantity);
 	}
 	
 	
 	
 	//리스트 페이지
-	public BookDTO(Long bid, String title, String image, String author, String publisher, Long price, String ad,
+	public BookDTO(Long id, String title, String coverImg, Long authorId, String publisher, Integer price, String catchphrase,
 			String introduce) {
-		this.bid = bid;
+		this.id = id;
 		this.title = title;
-		this.image = image;
-		this.author = author;
+		this.coverImg = coverImg;
+		this.authorId = authorId;
 		this.publisher = publisher;
 		this.price = price;
-		this.ad = ad;
+		this.catchphrase = catchphrase;
 		this.introduce = introduce;
 	}
 	
 	//리스트페이지 (슬라이드목록)
-	public BookDTO(Long bid, String title, String image, String author, String ad) {
-		this.bid = bid;
+	public BookDTO(Long id, String title, String coverImg, Long authorId, String catchphrase) {
+		this.id = id;
 		this.title = title;
-		this.image = image;
-		this.author = author;
-		this.ad = ad;
+		this.coverImg = coverImg;
+		this.authorId = authorId;
+		this.catchphrase = catchphrase;
 	}
 
 	// 장바구니페이지
-	public BookDTO(String image, String title, Long price, String estimatedDeliveryDate) {
-		super();
-		this.image = image;
+	public BookDTO(String coverImg, String title, Integer price, Integer estimatedDeliveryDate) {
+		this.coverImg = coverImg;
 		this.title = title;
 		this.price = price;
 		this.estimatedDeliveryDate = estimatedDeliveryDate;
 	}
 
-	public BookDTO(String image, String title, Long price) {
-		this.image = image;
+	public BookDTO(String coverImg, String title, Integer price) {
+		this.coverImg = coverImg;
 		this.title = title;
 		this.price = price;
-	
 	}
 
-	public Long getBid() {
-		return bid;
+	public Long getId() {
+		return id;
 	}
 
-	public void setBid(Long bid) {
-		this.bid = bid;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
-	public String getImage() {
-		return image;
+	public String getCoverImg() {
+		return coverImg;
 	}
 
-	public void setImage(String image) {
-		this.image = image;
+	public void setCoverImg(String coverImg) {
+		this.coverImg = coverImg;
 	}
 
 	public String getTitle() {
@@ -170,12 +132,12 @@ public class BookDTO {
 		this.title = title;
 	}
 
-	public String getAuthor() {
-		return author;
+	public Long getAuthorId() {
+		return authorId;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
 	}
 
 	public String getTranslator() {
@@ -186,59 +148,67 @@ public class BookDTO {
 		this.translator = translator;
 	}
 
-	public Long getPrice() {
+	public Integer getPrice() {
 		return price;
 	}
 
-	public void setPrice(Long price) {
+	public void setPrice(Integer price) {
 		this.price = price;
 	}
 
-	public Long getDeliveryFee() {
+	public Integer getDeliveryFee() {
 		return deliveryFee;
 	}
 
-	public void setDeliveryFee(Long deliveryFee) {
+	public void setDeliveryFee(Integer deliveryFee) {
 		this.deliveryFee = deliveryFee;
 	}
 
-	public String getEstimatedDeliveryDate() {
+	public Integer getEstimatedDeliveryDate() {
 		return estimatedDeliveryDate;
 	}
 
-	public void setEstimatedDeliveryDate(String estimatedDeliveryDate) {
+	public void setEstimatedDeliveryDate(Integer estimatedDeliveryDate) {
 		this.estimatedDeliveryDate = estimatedDeliveryDate;
 	}
 
-	public Long getTotal_pages() {
+	public Integer getTotalPages() {
 		return totalPages;
 	}
 
-	public void setTotal_pages(Long totalPages) {
+	public void setTotalPages(Integer totalPages) {
 		this.totalPages = totalPages;
 	}
 
-	public Long getWeight() {
+	public Integer getWeight() {
 		return weight;
 	}
 
-	public void setWeight(Long weight) {
+	public void setWeight(Integer weight) {
 		this.weight = weight;
 	}
 
-	public Long getIsbn() {
-		return isbn;
+	public Long getIsbn13() {
+		return isbn13;
 	}
 
-	public void setIsbn(Long isbn) {
-		this.isbn = isbn;
+	public void setIsbn13(Long isbn13) {
+		this.isbn13 = isbn13;
 	}
 
-	public String getBookCategoryId() {
+	public Long getIsbn10() {
+		return isbn10;
+	}
+
+	public void setIsbn10(Long isbn10) {
+		this.isbn10 = isbn10;
+	}
+
+	public Integer getBookCategoryId() {
 		return bookCategoryId;
 	}
 
-	public void setBookCategoryId(String bookCategoryId) {
+	public void setBookCategoryId(Integer bookCategoryId) {
 		this.bookCategoryId = bookCategoryId;
 	}
 
@@ -250,12 +220,12 @@ public class BookDTO {
 		this.introduce = introduce;
 	}
 
-	public String getIntroduceImage() {
-		return introduceImage;
+	public String getIntroduceImg() {
+		return introduceImg;
 	}
 
-	public void setIntroduceImage(String introduceImage) {
-		this.introduceImage = introduceImage;
+	public void setIntroduceImg(String introduceImg) {
+		this.introduceImg = introduceImg;
 	}
 
 	public String getPublisher() {
@@ -282,54 +252,51 @@ public class BookDTO {
 		this.contents = contents;
 	}
 
-	public Long getVisit() {
+	public Integer getVisit() {
 		return visit;
 	}
 
-	public void setVisit(Long visit) {
+	public void setVisit(Integer visit) {
 		this.visit = visit;
 	}
 
-	public String getPublicationDate() {
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getCatchphrase() {
+		return catchphrase;
+	}
+
+	public void setCatchphrase(String catchphrase) {
+		this.catchphrase = catchphrase;
+	}
+
+	public Date getPublicationDate() {
 		return publicationDate;
 	}
 
-	public void setPublicationDate(String publicationDate) {
+	public void setPublicationDate(Date publicationDate) {
 		this.publicationDate = publicationDate;
 	}
 
-	public String getRegistDate() {
+	public Date getRegistDate() {
 		return registDate;
 	}
 
-	public void setRegistDate(String registDate) {
+	public void setRegistDate(Date registDate) {
 		this.registDate = registDate;
 	}
 
-	public String getUpdateDate() {
+	public Date getUpdateDate() {
 		return updateDate;
 	}
 
-	public void setUpdateDate(String updateDate) {
+	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
 	}
-
-	public String getAd() {
-		return ad;
-	}
-
-	public void setAd(String ad) {
-		this.ad = ad;
-	}
-
-	public Integer getCount() {
-		return count;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-	
-	
-	
 }
--- BooksiteProject/src/main/java/org/iptime/booke/dto/MemberDTO.java
+++ BooksiteProject/src/main/java/org/iptime/booke/dto/MemberDTO.java
@@ -1,33 +1,37 @@
 package org.iptime.booke.dto;
 
+import java.sql.Date;
+
 public class MemberDTO {
+	private Long id;
 	private String name;
-	private String gender;
-	private String id;
+	private Short genderId;
 	private String password;
-	private String phoneNumber;
+	private String phoneNum;
 	private String address;
 	private String email;
+	private Date birth;
+	private Integer point;
+	private Short memberStateId;
+	private Date registerDate;
 	
-	public MemberDTO() {
-		
-	}
+	public MemberDTO() {}
 	
 	//결제페이지
 	
-	public MemberDTO(String name, String id, String phoneNumber, String address) {
+	public MemberDTO(String name, Long id, String phoneNum, String address) {
 		this.name = name;
 		this.id = id;
-		this.phoneNumber = phoneNumber;
+		this.phoneNum = phoneNum;
 		this.address = address;
 		
 	}
 	
-	public MemberDTO(String name,String id, String password, String phoneNumber, String address) {
+	public MemberDTO(String name, Long id, String password, String phoneNum, String address) {
 		this.name = name;
 		this.id = id;
 		this.password = password;
-		this.phoneNumber = phoneNumber;
+		this.phoneNum = phoneNum;
 		this.address = address;
 	}
 
@@ -41,11 +45,19 @@ public class MemberDTO {
 //		this.address = address;
 //	}
 	
-	public MemberDTO(String name,String id) {
+	public MemberDTO(String name, Long id) {
 		this.name = name;
 		this.id = id;
 	}
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -54,20 +66,12 @@ public class MemberDTO {
 		this.name = name;
 	}
 
-	public String getGender() {
-		return gender;
+	public Short getGenderId() {
+		return genderId;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
+	public void setGenderId(Short genderId) {
+		this.genderId = genderId;
 	}
 
 	public String getPassword() {
@@ -78,12 +82,12 @@ public class MemberDTO {
 		this.password = password;
 	}
 
-	public String getPhoneNumber() {
-		return phoneNumber;
+	public String getPhoneNum() {
+		return phoneNum;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
 	}
 
 	public String getAddress() {
@@ -94,14 +98,6 @@ public class MemberDTO {
 		this.address = address;
 	}
 
-//	public int getOrderQuantity() {
-//		return orderQuantity;
-//	}
-//
-//	public void setOrderQuantity(int orderQuantity) {
-//		this.orderQuantity = orderQuantity;
-//	}
-
 	public String getEmail() {
 		return email;
 	}
@@ -110,6 +106,36 @@ public class MemberDTO {
 		this.email = email;
 	}
 
+	public Date getBirth() {
+		return birth;
+	}
 
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public Integer getPoint() {
+		return point;
+	}
+
+	public void setPoint(Integer point) {
+		this.point = point;
+	}
+
+	public Short getMemberStateId() {
+		return memberStateId;
+	}
+
+	public void setMemberStateId(Short memberStateId) {
+		this.memberStateId = memberStateId;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
 	
 }
