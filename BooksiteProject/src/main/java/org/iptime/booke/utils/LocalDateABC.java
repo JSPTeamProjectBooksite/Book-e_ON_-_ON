e483ca2f9cd2d81ab3c7969d3afd6d3901020ccd
DKS2312 <dtg2312@gmail.com>
2022-10-12 16:48:39 +0900

    LocalDateABC 최종 수정 완료

--- BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
+++ BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
@@ -11,7 +11,6 @@ public class LocalDateABC {
 	public static LocalDate toLocalDate(Date date) {
 		return date.toLocalDate();
 	}
-	
 	// Date -> LocalDateTime
 	public static LocalDateTime toLocalDateTime(Date date) {
 		return new Timestamp(date.getTime()).toLocalDateTime();
@@ -21,32 +20,26 @@ public class LocalDateABC {
 	public static Date dateValueOf(LocalDate localDate) {
 		return Date.valueOf(localDate);
 	}
-	
 	// LocalDateTime -> Date
-	public static Date dateValueOf(LocalDateTime localDateTime) {
+	public static Date dateValueOf(LocalDateTime localDateTime) { 
 		return new Date(Timestamp.valueOf(localDateTime).getTime());
-	}
-	
-	
-	
-	
-	// LocalDate -> String
-	public static String printDate(LocalDate localDate) {
-		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-	}
-	
-	// LocalDateTime -> String
-	public static String printDate(LocalDateTime localDate) {
-		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 	}
 	
 	// String -> LocalDate
 	public static LocalDate stringToLocalDate(String strDate) {		
 		return LocalDate.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 	}
-	
 	// String -> LocalDateTime
 	public static LocalDateTime stringToLocalDateTime(String strDate) {
 		return LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+	
+	// LocalDate -> String
+	public static String printDate(LocalDate localDate) {
+		return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	}
+	// LocalDateTime -> String
+	public static String printDate(LocalDateTime localDate) {
+		return localDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 	}
 }
