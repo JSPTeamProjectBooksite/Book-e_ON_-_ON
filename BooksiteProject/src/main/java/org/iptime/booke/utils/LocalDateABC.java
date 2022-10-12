b46d4aa417b7472b12139ec5b892da18859e4762
DKS2312 <dtg2312@gmail.com>
2022-10-12 12:51:47 +0900

    LocalDateABC 클래스 주석 추가

--- BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
+++ BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
@@ -7,17 +7,22 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateABC {
+	// Date -> LocalDate
 	public static LocalDate toLocalDate(Date date) {
 		return date.toLocalDate();
 	}
 	
+	// Date -> LocalDateTime
 	public static LocalDateTime toLocalDateTime(Date date) {
 		return new Timestamp(date.getTime()).toLocalDateTime();
 	}
 	
+	// LocalDate -> String
 	public static String printLocalDate(LocalDate localDate) {
 		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
 	}
+	
+	// LocalDateTime -> String
 	public static String printLocalDateTime(LocalDateTime localDate) {
 		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
 	}
