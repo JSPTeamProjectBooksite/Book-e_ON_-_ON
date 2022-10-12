35506d111b433cbb92b38f93d1f937b3cc4947c7
DKS2312 <dtg2312@gmail.com>
2022-10-12 13:52:23 +0900

    LocalDateABC 업그레이드

--- BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
+++ BooksiteProject/src/main/java/org/iptime/booke/utils/LocalDateABC.java
@@ -17,13 +17,36 @@ public class LocalDateABC {
 		return new Timestamp(date.getTime()).toLocalDateTime();
 	}
 	
+	// LocalDate -> Date
+	public static Date dateValueOf(LocalDate localDate) {
+		return Date.valueOf(localDate);
+	}
+	
+	// LocalDateTime -> Date
+	public static Date dateValueOf(LocalDateTime localDateTime) {
+		return new Date(Timestamp.valueOf(localDateTime).getTime());
+	}
+	
+	
+	
+	
 	// LocalDate -> String
-	public static String printLocalDate(LocalDate localDate) {
+	public static String printDate(LocalDate localDate) {
 		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
 	}
 	
 	// LocalDateTime -> String
-	public static String printLocalDateTime(LocalDateTime localDate) {
+	public static String printDate(LocalDateTime localDate) {
 		return localDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+	}
+	
+	// String -> LocalDate
+	public static LocalDate stringToLocalDate(String strDate) {		
+		return LocalDate.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+	}
+	
+	// String -> LocalDateTime
+	public static LocalDateTime stringToLocalDateTime(String strDate) {
+		return LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 	}
 }
