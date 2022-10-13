77bb67cb640baab5920b6a2704ddb878690df59b
DKS2312 <dtg2312@gmail.com>
2022-10-13 13:10:44 +0900

    DB변경 : 문의 테이블 상태칼럼 추가

--- BooksiteProject/src/main/webapp/WEB-INF/sql/Setting/integrated_Setting.sql
+++ BooksiteProject/src/main/webapp/WEB-INF/sql/Setting/integrated_Setting.sql
@@ -134,6 +134,7 @@ CREATE TABLE inquiry_TBL(
 	title			varchar(500)	NOT NULL,
 	content 		varchar2(4000) 	NOT NULL,
 	categroy 		varchar2(100) 	NOT NULL,
+	state			varchar2(50)	DEFAULT '답변 대기중',
 	rigister_DATE 	DATE 			DEFAULT sysdate 
 );
 
@@ -144,6 +145,7 @@ COMMENT ON COLUMN inquiry_TBL.MEMBER_ID  IS '문의를 쓴 멤버 id';
 COMMENT ON COLUMN inquiry_TBL.TITLE  IS '문의 제목';
 COMMENT ON COLUMN inquiry_TBL.CONTENT  IS '문의 내용';
 COMMENT ON COLUMN inquiry_TBL.CATEGROY  IS '문의 카테고리 (버그, UI, 질문 등등)';
+COMMENT ON COLUMN inquiry_TBL.STATE  IS '답변 상태';
 COMMENT ON COLUMN inquiry_TBL.RIGISTER_DATE  IS '문의를 생성한 날짜';
 
 CREATE SEQUENCE inquiry_SEQ
