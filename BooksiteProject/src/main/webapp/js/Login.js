1c10711bf81c10f9e655c681677aa3b6d0795dbf
jinyoungchoi1003 <samcokz@naver.com>
2022-10-17 17:59:43 +0900

    커밋

--- BooksiteProject/src/main/webapp/js/Login.js
+++ BooksiteProject/src/main/webapp/js/Login.js
@@ -1,18 +1,27 @@
+function validateForm(form) {
+	if (!form.email.value) {
+		alert("이메일을 입력하세요.");
+		return false;
+	}
+	if (!form.password.value) {
+		alert("비밀번호을 입력하세요.");
+		return false;
+	}
+}
+
 const inputs = document.querySelectorAll(".input");
 
-
-function addcl(){
+function addcl() {
 	let parent = this.parentNode.parentNode;
 	parent.classList.add("focus");
 }
 
-function remcl(){
+function remcl() {
 	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
+	if (this.value == "") {
 		parent.classList.remove("focus");
 	}
 }
-
 
 inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
