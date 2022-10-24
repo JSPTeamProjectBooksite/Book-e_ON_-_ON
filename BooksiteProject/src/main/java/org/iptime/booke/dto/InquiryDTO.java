72eca4af7f2f124aabc2a17000261c865581891c
DKS2312 <dtg2312@gmail.com>
2022-10-24 14:50:44 +0900

    문의 dto수정

--- BooksiteProject/src/main/java/org/iptime/booke/dto/InquiryDTO.java
+++ BooksiteProject/src/main/java/org/iptime/booke/dto/InquiryDTO.java
@@ -6,7 +6,8 @@ public class InquiryDTO {
 	private Long id;
 	private Long memberId;
 	private String title;
-	private String content;
+	private String mContent;
+	private String aContent;
 	private String reply;
 
 	private String categroy;
@@ -15,77 +16,107 @@ public class InquiryDTO {
 
 	
 	public InquiryDTO() {}
-	
-	public InquiryDTO(Long id, Long memberId, String title, String content, String reply, String categroy, String state,
-			LocalDateTime rigisterDate) {
+
+
+	public InquiryDTO(Long id, Long memberId, String title, String mContent, String aContent, String reply,
+			String categroy, String state, LocalDateTime rigisterDate) {
 		super();
 		this.id = id;
 		this.memberId = memberId;
 		this.title = title;
-		this.content = content;
+		this.mContent = mContent;
+		this.aContent = aContent;
 		this.reply = reply;
 		this.categroy = categroy;
 		this.state = state;
 		this.rigisterDate = rigisterDate;
 	}
+
+
 	public Long getId() {
 		return id;
 	}
+
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
 	public Long getMemberId() {
 		return memberId;
 	}
+
 
 	public void setMemberId(Long memberId) {
 		this.memberId = memberId;
 	}
 
-	public String getReply() {
-		return reply;
-	}
-	
-	public void setReply(String reply) {
-		this.reply = reply;
-	}
+
 	public String getTitle() {
 		return title;
 	}
+
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	public String getContent() {
-		return content;
+
+	public String getmContent() {
+		return mContent;
 	}
 
-	public void setContent(String content) {
-		this.content = content;
+
+	public void setmContent(String mContent) {
+		this.mContent = mContent;
 	}
+
+
+	public String getaContent() {
+		return aContent;
+	}
+
+
+	public void setaContent(String aContent) {
+		this.aContent = aContent;
+	}
+
+
+	public String getReply() {
+		return reply;
+	}
+
+
+	public void setReply(String reply) {
+		this.reply = reply;
+	}
+
 
 	public String getCategroy() {
 		return categroy;
 	}
 
+
 	public void setCategroy(String categroy) {
 		this.categroy = categroy;
 	}
+
 
 	public String getState() {
 		return state;
 	}
 
+
 	public void setState(String state) {
 		this.state = state;
 	}
 
+
 	public LocalDateTime getRigisterDate() {
 		return rigisterDate;
 	}
+
 
 	public void setRigisterDate(LocalDateTime rigisterDate) {
 		this.rigisterDate = rigisterDate;
