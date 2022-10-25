77419c308d30fe1d05fac8b0d018499d186ae60b
DKS2312 <dtg2312@gmail.com>
2022-10-25 12:39:55 +0900

    알림페이지 개발 중단

--- BooksiteProject/src/main/java/org/iptime/booke/controller/NoticeController.java
+++ BooksiteProject/src/main/java/org/iptime/booke/controller/NoticeController.java
@@ -1,9 +1,9 @@
 package org.iptime.booke.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.iptime.booke.dto.PostDTO;
+import org.iptime.booke.dao.NoticeDAO;
+import org.iptime.booke.dto.NoticeDTO;
 
 /**
  * Servlet implementation class NoticeController
@@ -22,9 +23,33 @@ public class NoticeController extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		List<PostDTO> postList = daooo();
-		request.setAttribute("postList", postList);
+		Map<String, Object> param = new HashMap<String, Object>();
+		
+		NoticeDAO nDao = new NoticeDAO();
+		// 페이지 처리 부분
+		// 전체 페이지 수 계산
+		int totalCount = nDao.totalCount();
+		int pageSize = 20;
+		int blockPage = 10;
+		int totalPage = (int)Math.ceil((double)totalCount / pageSize);	// 페이지 수 = (게시글 전체 개수)/(한 페이지당 보여지는 개수) 반드시 올림해야 됨
+		
+		// 현재 페이지 확인
+		int pageNum = 1;
+		String pageTemp = request.getParameter("pageNum");
+		if(pageTemp != null && !pageTemp.equals("")) pageNum = Integer.parseInt(pageTemp);
+		
+		// 목록에 출력할 게시물 범위 계산
+		int start = (pageNum - 1) * pageSize + 1;
+		int end = pageNum * pageSize;
+		param.put("start", start);
+		param.put("end", end);
+		
+		List<NoticeDTO> noticeList = nDao.selectListPage(param);
+		request.setAttribute("noticeList", noticeList);
+		request.setAttribute("blockPage", blockPage);
+		request.setAttribute("totalPage", totalPage);
+		request.setAttribute("pageNum", pageNum);
+		request.setAttribute("pageSize", pageSize);
 
 		request.getRequestDispatcher("/NoticePage.jsp").forward(request, response);
 	}
@@ -32,16 +57,5 @@ public class NoticeController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-	}
-
-	private List<PostDTO> daooo() {
-		List<PostDTO> postList = new ArrayList<PostDTO>();
-		
-		Date date = new Date();
-
-		for(int i = 0; i < 15; i++)
-			postList.add(new PostDTO("이벤트", "올해 절대 놓치면 안되는 초특가 상품 top10", date));
-
-		return postList;
 	}
 }
