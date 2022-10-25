d3353f0a1e3a3fac6f9d5a05322bc075e046c597
jinyoungchoi1003 <samcokz@naver.com>
2022-10-25 18:00:59 +0900

    비밀번호 변경 완료 2st

--- BooksiteProject/src/main/java/org/iptime/booke/controller/UserInfoUpdateController.java
+++ BooksiteProject/src/main/java/org/iptime/booke/controller/UserInfoUpdateController.java
@@ -36,15 +36,16 @@ public class UserInfoUpdateController extends HttpServlet {
 
 		if (b == false) {
 			System.out.println("현재 비밀번호가 일치하지 않습니다.\n확인 후 다시 시도해주세요.");
+		} else if (userPw.equals(userNewPw)) {
+			System.out.println("기존 비밀번호와 동일한 비밀번호를 입력하셨습니다.\n다시 입력해주세요.");
+		} else if (!userNewPw.equals(userPwCheck)) {
+			System.out.println("새 비밀번호와 새 비밀번호 확인이 다릅니다.");
 		} else {
 			int iResult = dao.updateUserEdit(dto);
 			if (iResult == 1) {
 				System.out.println("회원정보 수정 성공!!!");
 				System.out.println("비밀번호가 성공적으로 변경되었습니다. 다시 로그인 해주세요.");
 				request.setAttribute("message", "비밀번호가 성공적으로 변경되었습니다. 다시 로그인 해주세요.");
-			} else {
-				System.out.println("회원정보 수정 실패!!!");
-				request.setAttribute("message", "회원정보 수정 실패!!!");
 			}
 			request.getRequestDispatcher("/PrintString.jsp").forward(request, response);
 		}
